1a5eb1860db2f8caf333a09849c9af77a328bd22
Devvrat53 <devvratmungekar53@gmail.com>
2020-05-31 18:15:32 +0530

    update

--- app.py
+++ app.py
@@ -12,18 +12,10 @@ def home():
 @app.route('/predict', methods= ['POST'])
 def predict():
     int_features = [int(x) for x in request.form.values()]
-    #print(int_features, len(int_features))
     final = [np.array(int_features)]
-    #print(final)
     prediction = model.predict(final)
     output = prediction
-    #print(output)
     return render_template('index.html', prediction_text= output)
-    '''
-    if output == [1]:
-        return render_template('index.html', prediction_txt= 'You have a Cardio-vascular disease')
-    else:
-        return render_template('index.html', prediction_txt= 'You do not Cardio-vascular disease')
-'''    
+
 if __name__ == '__main__':
     app.run(debug= True)
